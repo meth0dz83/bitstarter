583e3dc31704c1a47353d0796b146f4b7ef83482
juan flores <juanflores@outlook.com>
2013-07-08 00:36:08 +0000

    fix web.js

--- web.js
+++ web.js
@@ -5,8 +5,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = new Buffer( fs.readFileSync("index.html") );  
-  response.send(buffer.toString('utf-8');
-);
+  response.send(buffer.toString('utf-8'));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
